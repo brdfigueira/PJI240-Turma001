30761eadf8787b4f2d0851eaba24de29834f02cb
LabCJ <131810156+alnardim@users.noreply.github.com>
2023-09-27 23:06:24 -0300

    Formulario de triagem
    Trabalhando no POST do formulario / criaçao das entradas no banco

--- Django/advDig/triagem/views.py
+++ Django/advDig/triagem/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
-from .models import Usuario
+from .models import Usuario, Demanda, FormUsuario, FormDemanda
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -10,7 +10,33 @@ def home(request):
     return render(request, 'home/index.html')
 
 def demandaNovo(request):
-    return render(request, 'home/index.html')
+    usuario = auth.get_user(request)
+    if usuario.is_authenticated:
+        pk = usuario.pk
+        usuario = Usuario.objects.get(pk=pk)
+        nome = usuario.first_name
+        messages.add_message(request, messages.SUCCESS, f"Você está logado como {nome}. Se esse não for você, por favor faça logoff e tente novmente.")
+        return redirect('home')
+
+    if request.method != 'POST':
+        form1 = FormUsuario(prefix="form1")
+        form2 = FormDemanda(prefix="form2")
+        contexto = {'form1': form1, 'form2': form2}
+        return render(request, 'triagem/demanda.html', contexto)
+
+    user = FormUsuario(request.POST, prefix="form1")
+    demanda = FormDemanda(request.POST, prefix="form2")
+    user = user.save(commit=False)
+    user.username = user.email
+    print(user.username)
+    user.save()
+    demanda = demanda.save(commit=False)
+    demanda.usuario = user
+    demanda.save()
+
+    messages.add_message(request, messages.SUCCESS, "Postado?")
+
+    return render(request, 'triagem/demanda.html')
 
 def demandaCliente(request):
     request.session['demanda'] = True
